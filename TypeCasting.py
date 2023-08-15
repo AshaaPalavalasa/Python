1629a09a4c21305f8244de18bd19f65cdbf97230
Ashaa Palavalasa <ashaapalavalasa5532@gmail.com>
2023-08-15 07:25:47 +0530

    typecasting

--- TypeCasting.py
+++ TypeCasting.py
@@ -1,6 +1,10 @@
-### TypeCasting is converting the datatype of a valid value to another ###
+### TypeCasting is converting the datatype of a valid value to another --> throws an error if the value is not valid ###
 a="1"
 b="5"
 print(a+b)                  # 15 but requried is 6
-print(int(a)+int(b))        # 6
 """Explicit Typecasting: Conversion is done by the user"""
+print(int(a)+int(b))        # 6         #str(),int(),float(),bool()..etc
+"""Implicit TypeCasting: Python Converts smaller dataype to higher dataype automatically"""
+c=10.5
+d=10
+print(c+d)                  # d is int, which is automatically converted into float
---
+++ UserInput.py
@@ -0,0 +1,5 @@
+## Input can be taken by using input() function. This function returns value as a string/character ##
+a=input("Enter a Value ")            #input 3
+print(type(a))                       #str
+a=int(a)
+print(type(a))                       #int
